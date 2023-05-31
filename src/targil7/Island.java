ff58479797249559a4ed48e1ed5df639ea0ed0f0
LeaScetbon <73163251+LeaScetbon@users.noreply.github.com>
2023-05-31 16:50:41 +0300

    name

--- src/targil7/Island.java
+++ src/targil7/Island.java
@@ -2,16 +2,17 @@ package targil7;
 // TODO: Implement Composite (change this file).
  
 public class Island extends Element {
-
+    private String name;
     public Island(String name, double diameter, String path) {
         //TODO: fix
         super(0,0,null);
+        this.name = name;
     }
 
     @Override
     public String getName() {
         //TODO: fix
-        return null;
+        return this.name;
     }
 
     @Override
--- src/targil7/Lake.java
+++ src/targil7/Lake.java
@@ -1,18 +1,19 @@
 package targil7;
 
 public class Lake extends Element {
-
+    private String name;
     public Lake(String name, double diameter, String path) {
         //TODO: fix
         super(diameter, diameter, path);
         String lake_name = name;//Comment envoyer name à getName() ?
         //super(0,0,null);
+        this.name = name;
     }
 
     @Override
     public String getName() {
         //TODO: fix
-        return null;
+        return this.name;
     }
 
     @Override
